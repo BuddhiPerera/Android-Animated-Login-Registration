a431eaa0e2bf01f4f17a9b69d8bbfc092d846c48
Buddhi Perera <67018688+BuddhiPerera@users.noreply.github.com>
2022-09-05 15:52:50 +0530

    Login UI Animation

--- app/src/main/java/com/example/sample/LoginActivity.java
+++ app/src/main/java/com/example/sample/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.sample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // changing status bar color
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View view){
+        startActivity(new Intent((this, RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
