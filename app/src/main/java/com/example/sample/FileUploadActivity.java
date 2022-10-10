ec30e4c018b40fadfc9937dd0ac95d525dd29f6c
rivindu2 <rivindu@incubatelabs.com>
2022-10-10 13:00:57 +0530

    ADd Upload

--- app/src/main/java/com/example/sample/FileUploadActivity.java
+++ app/src/main/java/com/example/sample/FileUploadActivity.java
@@ -1,72 +1,50 @@
 package com.example.sample;
 
-import static android.content.ContentValues.TAG;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Base64;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
-
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Base64;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.bumptech.glide.Glide;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpEntity;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.HttpResponse;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.ClientProtocolException;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.HttpClient;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.client.methods.HttpPost;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.impl.client.DefaultHttpClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class FileUploadActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     ImageView imageView;
     Button camera, gallery;
-    String uri ;
-    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    private ProgressBar loadingPB;
     private JSONObject jsonObject;
     private RequestQueue requestQueue;
     private JsonObjectRequest jsonObjectRequest;
+    private ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,14 +56,12 @@ public class FileUploadActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(FileUploadActivity.this);
         progressDialog.setMessage("Image Uploading...");
 //Add permission for camera
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa");
         camera.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYyyy");
+
                 if (CheckPermission()) {
-                    System.out.println("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUuuu");
                     Intent capture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                     startActivityForResult(capture, 0);
                 }
@@ -102,6 +78,7 @@ public class FileUploadActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -145,11 +122,6 @@ public class FileUploadActivity extends AppCompatActivity {
 
     }
 
-    Uri u;
-
-
-
-    private ProgressDialog progressDialog;
     private void UploadImage(Bitmap bitmap) {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
@@ -194,7 +166,7 @@ public class FileUploadActivity extends AppCompatActivity {
     }
 
     public boolean CheckPermission() {
-        System.out.println("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTtttt");
+        System.out.println("00000000000000000000000000000000");
         if (ContextCompat.checkSelfPermission(FileUploadActivity.this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(FileUploadActivity.this,
@@ -231,7 +203,6 @@ public class FileUploadActivity extends AppCompatActivity {
                         .create()
                         .show();
 
-
             } else {
                 System.out.println("555555555555555555555555555555555555555555555");
                 ActivityCompat.requestPermissions(FileUploadActivity.this,
@@ -248,37 +219,4 @@ public class FileUploadActivity extends AppCompatActivity {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putParcelable("file_uri", u);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        u = savedInstanceState.getParcelable("file_uri");
-    }
-
-    public void pick_from_gallery(View view) {
-        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(pickPhoto, 1);
-    }
 }
