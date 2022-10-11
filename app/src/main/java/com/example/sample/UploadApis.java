718883a38ec3b66dcde159c000df1019d93178f9
rivindu2 <rivindu@incubatelabs.com>
2022-10-11 08:11:45 +0530

    Error With Response

---
+++ app/src/main/java/com/example/sample/UploadApis.java
@@ -0,0 +1,21 @@
+package com.example.sample;
+
+import com.example.sample.model.Post;
+
+import okhttp3.MultipartBody;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Headers;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+public interface UploadApis {
+    @Multipart
+    @Headers({
+            "Content-Type: application/json"
+    })
+    @POST("upload")
+    Call<ResponseBody> uploadImage(@Part MultipartBody.Part image);
+}
